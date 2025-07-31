54e919e1bf2570499956f99a2b6f8a1c39320276
job.yuraab@gmail.com <job.yuraab@gmail.com>
2025-07-31 12:41:00 -0400

    Skip 3rd project dependency

---
+++ Models/HardwareDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Management;
+
+namespace HardwareDetection;
+
+public static class GpuDetector
+{
+    public static bool HasNvidiaGpu(out string? name)
+    {
+        name = null;
+
+        try
+        {
+            using var searcher = new ManagementObjectSearcher("select * from Win32_VideoController");
+            foreach (var obj in searcher.Get())
+            {
+                var gpuName = obj["Name"]?.ToString()?.ToLowerInvariant();
+                if (gpuName != null && gpuName.Contains("nvidia"))
+                {
+                    name = gpuName;
+                    return true;
+                }
+            }
+        }
+        catch { }
+
+        return false;
+    }
+}
+
